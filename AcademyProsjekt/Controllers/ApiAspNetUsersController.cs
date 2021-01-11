a4f5ce08a2d589dffa878680b6d57c2bcad7c7b3
LisaFosH <lisafosse@gmail.com>
2021-01-11 11:38:19 +0100

    Added APINetUserController

---
+++ AcademyProsjekt/Controllers/ApiAspNetUsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AcademyProsjekt.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class ApiAspNetUsersController : Controller
+	{
+		public JsonResult Index()
+		{
+            return new JsonResult (new { username = User.Identity.Name});
+		}
+        
+    }
+}
